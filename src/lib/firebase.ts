db460a42e27a1eafecb35b871e1a6b2c8204a468
Aditya Anand <adityaanand@Adityas-MacBook-Pro.local>
2025-04-18 18:32:50 +0530

    removed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,15 +12,6 @@ import {
   onAuthStateChanged
 } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAWGZ3Sq2qTIO5aH1mRkg9XIXPlJmujHFw",
-//   authDomain: "dev-qa-93069.firebaseapp.com",
-//   projectId: "dev-qa-93069",
-//   storageBucket: "dev-qa-93069.firebasestorage.app",
-//   messagingSenderId: "869126019569",
-//   appId: "1:869126019569:web:6bf9d2c3c063ba40c1cc50"
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
